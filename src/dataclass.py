6e924baf7792decbb97c3688ed11c33b4ec09668
thomas-rocke <45517493+thomas-rocke@users.noreply.github.com>
2022-04-20 20:15:29 +0100

    flake8 formatting

--- src/dataclass.py
+++ src/dataclass.py
@@ -1,28 +1,29 @@
 from json_handler import Json_handler
 
 
-
-
-      
-
-
 class Data_class():
-  '''!
-  Class to hold data for file I/O, binding to F90 solvers, and visualisation
-  '''
-
-  def __init__(self):
     '''!
-    Define attribute namespaces
-    
+    Class to hold data for file I/O, binding to F90 solvers, and visualisation
     '''
+    _jh = Json_handler()
 
-    self.input_data = {
-      "grid_type"  : "r",
-      "grid_level" : 2,
-      "A"          : 1,
-      "L"          : 1,
-      "M"          : 1,
-      "p0"         : 0,
-      "p1"         : 0 
-    }
+    def __init__(self):
+        '''!
+        Define attribute namespaces
+
+        '''
+
+        self.input_data = self._jh.get_rundata("default")
+
+    def get_rundata(self, run_name: str) -> None:
+        '''!
+        Retrieve input run data from JSON file
+        '''
+        self.input_data = self._jh.get_rundata(run_name)
+
+    def save_rundata(self, run_name: str) -> None:
+        '''!
+        Save current input data as a new run in JSON file.
+        Will overwrite if run with the same name exists
+        '''
+        self._jh.set_rundata(run_name, self.input_data)
--- src/json_handler.py
+++ src/json_handler.py
@@ -2,75 +2,86 @@ import json
 import glob
 import os
 
+
 class Json_handler():
-  '''!
-  Singleton handler for JSON I/O
-
-  '''
-  _instance = None # Start with no instances of this class
-  _input_fname = "input-data.json" # fname of JSON file
-
-  def __new__(cls):
-    if (cls._instance is None):
-      # Instance does not exist already, create one
-      cls._instance = super().__new__(cls)
-      cls._instance._load_data()
-    return cls._instance
-
-  def _load_data(self) -> None:
     '''!
-    Loads data from self._input_fname JSON file
+    Singleton handler for JSON I/O
+
     '''
-    self.cwd = os.getcwd()
-    self.filepath = glob.glob("".join([self.cwd, os.sep, "**", os.sep, self._input_fname]), recursive = True)[0] # Recursive search to find file
-    self._data = json.loads(open(self.filepath).read())
+    _instance = None  # Start with no instances of this class
+    _input_fname = "input-data.json"  # fname of JSON file
 
+    def __new__(cls):
+        if (cls._instance is None):
+            # Instance does not exist already, create one
+            cls._instance = super().__new__(cls)
+            cls._instance._load_data()
+        return cls._instance
 
-  def save_data(self) -> None:
-    '''!
-    Save local run data back to json file
-    '''
-    with open(self.filepath, 'w') as f:
-      json.dump(self._data, f)
-  
-  @property
-  def run_names(self) -> list:
-    '''!
-    Get names of all available runs
-    '''
-    return list(self._data.keys())
+    def _load_data(self) -> None:
+        '''!
+        Loads data from self._input_fname JSON file
+        '''
+        self.cwd = os.getcwd()
+        # Recursive search to find file
+        self.filepath = glob.glob((self.cwd + os.sep + "**" + os.sep +
+                                   self._input_fname), recursive=True)[0]
+        self._data = json.loads(open(self.filepath).read())
 
-  def get_rundata(self, run_name:str) -> dict:
-    try:
-      return self._data[run_name]
-    except KeyError:
-      print(f"Run {run_name} not found.")
-      print(f"Currently recognised runs:")
-      [print(key) for key in self.get_run_names()]
-      return {}
+    def save_data(self) -> None:
+        '''!
+        Save local run data back to json file
+        '''
+        with open(self.filepath, 'w') as f:
+            json.dump(self._data, f)
 
-  def set_rundata(self, run_name:str, rundata:dict):
-    self._data[run_name] = rundata
+    @property
+    def run_names(self) -> list:
+        '''!
+        Get names of all available runs
+        '''
+        return list(self._data.keys())
+
+    def get_rundata(self, run_name: str) -> dict:
+        '''!
+        Search for a run with name matching run_name
+        Throws KeyError if name not found
+
+        '''
+        try:
+            return self._data[run_name]
+        except KeyError:
+            print(f"Run {run_name} not found.")
+            print("Currently recognised runs:")
+            print(self.get_run_names())
+            raise  # Raise the caught KeyError again
+
+    def set_rundata(self, run_name: str, rundata: dict):
+        '''!
+        Write new/overwrite data associated with run_name
+
+        '''
+        self._data[run_name] = rundata
+        self.save_data()
 
 
 def generate_json_file():
-  '''!
-  Generates a fresh json file with sample input configs
-  
-  '''
+    '''!
+    Generates a fresh json file with sample input configs
 
-  data = {}
+    '''
 
-  data["default"] = {
-      "grid_type"  : "r",
-      "grid_level" : 2,
-      "A"          : 1,
-      "L"          : 1,
-      "M"          : 1,
-      "p0"         : 0,
-      "p1"         : 0 
+    data = {}
+
+    data["default"] = {
+        "grid_type": "r",
+        "grid_level": 2,
+        "A": 1,
+        "L": 1,
+        "M": 1,
+        "p0": 0,
+        "p1": 0
     }
 
-
-  with open(Json_handler._input_fname, 'x') as f:
-    json.dump(data, f)
+    with open(Json_handler._input_fname, 'x') as f:
+        json.dump(data, f)
