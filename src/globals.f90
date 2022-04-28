240c51b7cf6ab8c3a2ed4bcbd9de22cd30cde5a5
Thomas Rocke <45517493+thomas-rocke@users.noreply.github.com>
2022-04-28 14:12:13 +0100

    Input Param verification

--- src/globals.f90
+++ src/globals.f90
@@ -20,6 +20,12 @@ module globals
   character(*), parameter :: logfile_prefix = "CH"
   character(*), parameter :: logfolder = "logs/"
 
+  interface to_string
+    module procedure real_to_string
+    module procedure int_to_string
+  end interface to_string
+
+
   contains
 
   !> @Brief Initialise logging
@@ -59,12 +65,128 @@ module globals
   !! @param[in]  CH_params [L, A, M, K, p0, p1]
   !! @param[in]  grid_init Grid initialisation type character
   !! @param[in]  grid_level Controls size of grid
-  !! @todo input validation 
+  !! @todo grid_level validation
   subroutine validate_params(CH_params, grid_init, grid_level)
     real(kind=dp), intent(in) :: CH_params(6)
-    character(1), intent(in) :: grid_init
+    character(*), intent(in) :: grid_init
     integer, intent(in) :: grid_level
 
+    real(dp) :: L, A, M, K, p0, p1
+
+    logical :: errors
+
+    real(dp), parameter :: REAL_TOL = 1e-10_DP
+
+    character(*), parameter :: ACCEPTED_INITS = "r"
+
+
+    errors = .FALSE.
+
+    L = CH_params(1)
+    A = CH_params(2)
+    M = CH_params(3)
+    K = CH_params(4)
+    p0 = CH_params(5)
+    p1 = CH_params(6)
+
+
+    ! Grid init char validation
+    if (len(grid_init) /=1) then
+      call logger%error("validate_params", "Grid init character should be of length 1, got "&
+                  // "length "//adjustl(trim(to_string(len(grid_init)))))
+      errors = .TRUE.
+    end if
+
+    if (verify(grid_init, ACCEPTED_INITS) /= 0) then
+      ! Verify() returns 0 if all chars of grid_init are in ACCEPTED_INITS
+      call logger%error("validate_params", "Grid init char '"//grid_init//"' not in accepted "&
+                  // "list of init chars: "//ACCEPTED_INITS)
+      errors = .TRUE.
+    end if
+
+    ! Grid level validation
+    if (grid_level<0) then
+      call logger%error("validate_params", "Grid level cannot be negative")
+      call logger%error("validate_params", "Got Grid level ="//adjustl(trim(to_string(grid_level))))
+      errors = .TRUE.
+    end if
+
+    ! L param validation
+    if (L<0) then
+      call logger%error("validate_params", "L cannot be negative")
+      call logger%error("validate_params", "Got L="//adjustl(trim(to_string(L))))
+      errors = .TRUE.
+    end if
+
+    ! A param validation
+    if (A<=0) then
+      call logger%error("validate_params", "A cannot be negative")
+      call logger%error("validate_params", "Got A="//adjustl(trim(to_string(A))))
+      errors = .TRUE.
+    end if
+
+    ! M param validation
+    if (M<0) then
+      call logger%error("validate_params", "M cannot be negative")
+      call logger%error("validate_params", "Got M="//adjustl(trim(to_string(M))))
+      errors = .TRUE.
+    end if
+    if (abs(M) < REAL_TOL) then
+      call logger%warning("validate_params", "M=0 implies system will not evolve")
+      call logger%warning("validate_params", "abs("//adjustl(trim(to_string(M)))&
+                //") was less "//"than tolerance of "//adjustl(trim(to_string(REAL_TOL))))
+    end if
+
+    ! K param validation
+    if (K<0) then
+      call logger%error("validate_params", "K cannot be negative")
+      call logger%error("validate_params", "Got K="//adjustl(trim(to_string(K))))
+      errors = .TRUE.
+    end if
+
+    ! p0 param validation
+
+
+    ! p1 param validation
+
+    if (abs(p0 - p1) < REAL_TOL) then
+      call logger%error("validate_params", "p0 and p1 cannot be equal")
+
+      call logger%error("validate_params", "Got p0="//adjustl(trim(to_string(p0)))//" and p1="&
+                      //adjustl(trim(to_string(p1))))
+
+      call logger%error("validate_params", "abs("//adjustl(trim(to_string(p0)))//" - "//adjustl(trim(to_string(p1)))&
+                        //") was less "//"than tolerance of "//adjustl(trim(to_string(REAL_TOL))))
+      errors = .TRUE.
+    end if
+
+
+    ! Check for any errors
+
+    if (errors) then
+      call logger%fatal("validate_params", "Issues found with input parameters")
+      stop
+    end if
+
+    call logger%trivia("validate_params", "No issues found in input parameters")
   end subroutine
+
+
+
+  function real_to_string(val) result(str)
+    real(dp), intent(in) :: val
+    character(128) :: str
+
+    write(str, *) val
+
+  end function real_to_string
+
+  function int_to_string(val) result(str)
+    integer, intent(in) :: val
+    character(128) :: str
+
+    write(str, *) val
+
+  end function int_to_string
 
 end module globals
