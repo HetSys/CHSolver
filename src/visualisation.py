48337a34ec10531e73e305936bb43f766d12108e
Matt N <95306381+NUMA21@users.noreply.github.com>
2022-05-19 17:45:28 +0100

    Visualisation output mp4 and png to main dir

--- src/visualisation.py
+++ src/visualisation.py
@@ -8,69 +8,88 @@ Opening file docstring contains LATEX parsing - \f$\beta\f$
 #     random docs
 #     \f$\alpha\f$
 
+from tkinter import N
 import h5py
 import matplotlib.pyplot as plt
 import matplotlib.animation as anim
 import glob
 import os
 import numpy as np
+
+def read_metadata(filename):
+  with open(filename) as f:
+
+    grid_params = f.readline().split()[1:]
+    sys_params = f.readline().split()[1:]
+    sys_params = sys_params + f.readline().split()[1:]
+
+  chkpnt_times = np.genfromtxt(filename, skip_header=4)
+
+  return np.array(grid_params, dtype = int), np.array(sys_params, dtype = float), np.array(chkpnt_times[:,1], dtype = float)
+
+
 Nchkpnts = len(os.listdir('out/')) -1
 grid_res = h5py.File('out/1.chkpnt', 'r')['c'][...].shape[0]
-
+grid_params,sys_params,t_array = read_metadata('out/metadata.dat')
+dimension = grid_params[0]
+gridres = 2**grid_params[1]
+L,A,M,K,p0,p1 = sys_params
 c = np.zeros((Nchkpnts,grid_res,grid_res))
 c_prev = np.zeros((Nchkpnts,grid_res,grid_res))
 dt = np.zeros((Nchkpnts))
+
 for i in range(1,Nchkpnts):
     data = h5py.File('out/'+str(i)+'.chkpnt', 'r')
     test= data['c'][...]
     c[i,:,:] = test
-    dt[:] = data['dt'][...]
+    dt[i] = data['dt'][...]
 
 ims =[]
 fig, ax = plt.subplots()
 
-for k in range(0, len(c)):
+for k in range(0, Nchkpnts):
     im = []
     shw = ax.imshow(c[k,:,:])
     im.append(shw)
-    im.append(plt.text((len(c[0,:,:])-1)/2,-3*len(c[0,:,:])/15 , s = ("T = " + str(k+1)), ha = "center", weight = "bold"))
+    im.append(plt.title('Evolution of the concentration', weight = "bold"))
+    im.append(plt.text(grid_res*0.425,grid_res*1.1 , s = ("T = " + str(t_array[k])), weight = "bold"))
     ims.append(im)
+
 fig.colorbar(shw)
-
-final_an = anim.ArtistAnimation(fig, ims, interval = 50, repeat = True)
+final_an = anim.ArtistAnimation(fig, ims, interval = 5, repeat = True)
+final_an.save('Conc_Evolution.mp4', fps = 10)
 plt.gca().set_aspect('equal', adjustable='box')
-
 plt.axis('off')
-plt.show()
+
+plt.clf()
+
+f_c = np.zeros((Nchkpnts,grid_res,grid_res)) #Bulk free energy density 
+for t in range(Nchkpnts):
+  for i in range(grid_res):
+    for j in range(grid_res):
+        f_c[t,i,j] = A*((c[t,i,j]-p0)**2)*(c[t,i,j]-p1)**2 
+F = np.zeros((Nchkpnts)) #Free energy functional
 
 
+c_halo = np.zeros((Nchkpnts, grid_res+2, grid_res+2))
+c_halo[:, 1:-1, 1:-1] = c
+c_halo[:,0,:] = c_halo[:,-2,:]
+c_halo[:,-1,:] = c_halo[:,1,:]
+c_halo[:,:,0] = c_halo[:,:,-2]
+c_halo[:,:,-1] = c_halo[:,:,1]
+for t in range(Nchkpnts):
+  grad_c_x = np.gradient(c_halo[t,:,:], axis=0)[1:-1,1:-1]
+  grad_c_y = np.gradient(c_halo[t,:,:], axis=1)[1:-1,1:-1]
+  grad_c_sqd = (grad_c_x ** 2 + grad_c_y ** 2)
+  for i in range(grid_res):
+    for j in range(grid_res):
+       F[t] += f_c[t,i,j] + 0.5*K*grad_c_sqd[i,j]
+F = F/(grid_res**2)
 
-# # f_c = np.zeros((grid_size,grid_size,len(t))) #Bulk free energy density 
-# # a = [2,6,9]#polynomial coefficients
-# # for t in range(len(time)):
-# #   for i in range(grid_size):
-# #     for j in range(grid_size):
-# #       f_c(i,j,t) = 0
-# #       for n in range(len(a)):
-# #         f_c(i,j,t) += a[n]*c[i,j,t]**(n-1)
+plt.plot(t_array,F)
 
-# # F = np.zeros((len(t))) #Free energy functional
-
-
-# # for t in range(len(time)):
-# #   for i in range(grid_size):
-# #     for j in range(grid_size):
-# #       F(t) += f_c(i,j,t) + 0.5*K*np.linalg.norm(np.gradient(c(i,j,t)))*dA 
-
-
-
-# # # plt.text(count/2-0.5, -(count/15+count/25), s = dat.initial, ha = "center")
-# # # plt.text(count/2-0.5, -(count/15+2*count/25), s = "N = " + str(dat.n) + ", T = " + str(dat.t) + ", J = " + str(dat.j) + ", B = " + str(dat.b), ha = "center")
-# # # plt.text(count/2-0.5, -(count/15+3*count/25), s = "Compiled from: " + dat.file, ha = "center")
-# # # plt.title(dat.title, weight = "bold")
-# # # plt.xlabel("Time (" + mag.tunits +")")
-# # # plt.ylabel("Magnetisation (" + mag.eunits +")")
-# # # plt.title("Average spin versus time", weight = "bold")
-# # grid_size = 3
-
+plt.title('Free Energy Functional', weight = "bold")
+plt.ylabel('Energy', weight = "bold")
+plt.xlabel('Time', weight = "bold")
+plt.savefig('Free_Energy.png')
 
