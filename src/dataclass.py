e3b6288f928d29340d276cbc3e941abbbcdb5f8e
ZiadFakhoury <ziad.fakhoury@warwick.ac.uk>
2022-05-23 12:27:19 +0100

    Update dataclass.py

--- src/dataclass.py
+++ src/dataclass.py
@@ -11,22 +11,24 @@ class CHData():
     input_data = {}
     _data = {}
 
-    L = 1.0
-    A = 1.0
-    M = 0.25
-    K = 0.0004
-    p0 = -1.0
-    p1 = 1.0
-    T = np.linspace(0, 0.1, 3)
-
-    grid_type = "r"
-    grid_level = 7
 
     def __init__(self, fname="input-data.json"):
         '''!
         Loads data from JSON file given by fname
         Will not save old files before opening a new one
         '''
+        
+        self.L = 1.0
+        self.A = 1.0
+        self.M = 0.25
+        self.K = 0.0004
+        self.p0 = -1.0
+        self.p1 = 1.0
+        self.T = np.linspace(0, 0.1, 3)
+
+
+        self.grid_type = "r"
+        self.grid_level = 7
 
         self.fname = fname
 
@@ -37,6 +39,8 @@ class CHData():
             self._read_jsonfile()
         else:
             self.save_rundata("default")
+            
+
 
 
     def _read_jsonfile(self):
