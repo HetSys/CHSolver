5250e83a217495e2880f4f478a2ba1604949448a
JeremyThorn <jezza.thorn@gmail.com>
2022-05-25 16:38:16 +0100

    Made timesteps more conservative.

--- src/pseudo_spectral_solver.f90
+++ src/pseudo_spectral_solver.f90
@@ -84,8 +84,8 @@ contains
       caseflag = 1
     end if
 
-    dt1 = 0.45_dp*kappa
-    dt2 = 0.45_dp/real(N, dp)
+    dt1 = 0.3_dp*kappa
+    dt2 = 0.3_dp/real(N, dp)
 
     if ( dt1 < dt2 ) then
       dt = dt1
