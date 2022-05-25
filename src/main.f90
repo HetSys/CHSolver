5c0c3d5a6ef2b0b51208b7fd55f5c3596cd9383c
Thomas Rocke <45517493+thomas-rocke@users.noreply.github.com>
2022-05-25 16:23:27 +0100

    Simplify main by collecting into subroutines

--- src/main.f90
+++ src/main.f90
@@ -34,6 +34,11 @@ program main
   ! start up MPI comms
   call comms_init()
 
+
+  !==========!
+  ! DEFAULTS !
+  !==========!
+
   selected_solver = SOLVER_FD_SELECTED ! Default to ps solver
   do_restart = .false.
   mpi_t = 0.0_dp
@@ -56,7 +61,7 @@ program main
     ! Parse CLI, initialise logging
     call initialise()
 
-    ! Get solver selection
+    ! Get solver selection & validate
     call get_selected_solver(selected_solver)
     call nproc_validate(selected_solver)
 
@@ -65,15 +70,7 @@ program main
 
     if (do_restart) then
       ! Should restart from a checkpoint
-      if (checkpoint_number > 0) then
-        call chkpnt_init(outdir, n, ch_params, t0, ierr, n_chkpnt=checkpoint_number)
-      else if (restart_time >= 0.0_dp) then
-        call chkpnt_init(outdir, n,  ch_params, t0, ierr, start_before_time=restart_time)
-      endif
-      allocate(c0(n, n))
-      allocate(c1(n, n))
- 
-      call read_hdf5_chkpnt(c0, c1, dt, ierr)
+      call read_from_checkpoint()
     endif
 
     ! Validate input params
@@ -87,7 +84,7 @@ program main
   else
     grid_res = 0
   endif
-
+  call MPI_Barrier(MPI_COMM_WORLD, ierr)
   ! send setup to other procs
   n = 2**grid_res
   call broadcast_setup(CH_params, Tout, c0, n, do_restart, t0, selected_solver)
@@ -99,36 +96,11 @@ program main
 
   ! call solver (on all procs)
   if (.not. do_restart) then
-    if (myrank == 0) then
-      ! initial concentration
-      call setup_grid(c0, grid_res, ch_params, init)
-
-      ! set up HDF5 outputting
-      call output_init(outdir, [2, grid_res], CH_params, ierr)
-    endif
-    
-    call MPI_Barrier(MPI_COMM_WORLD, ierr)
-    call solver_1(Tout, c0, CH_params, selected_solver, ierr)
+    ! Setup the grid from an initialisation character
+    ! Solve and output
+    call start_from_scratch()
   else
-
-    if(myrank .ne. 0) then
-      allocate(c1(n, n))
-    end if 
-
-    ! ensure that Tout(1) >= t0
-    do st_tout=1,size(tout)
-      if (tout(st_tout) > t0) then
-        exit
-      endif
-    enddo
-    call MPI_Barrier(MPI_COMM_WORLD, ierr)
-
-    ! Mask time array to only include future times
-    allocate(updated_tout(size(tout)-st_tout))
-    updated_tout = Tout(st_tout:)
-
-    ! Call solver_2 to restart with explicit C and C_prev
-    call solver_2(t0, updated_tout, c0, c1, dt, CH_params, selected_solver, ierr)
+    call start_from_checkpoint()
   endif
 
 
@@ -191,8 +163,56 @@ program main
   end subroutine
 
 
+  !> @brief read new input data from checkpointed info
+  !! Also initialise initial conditions of C and C_prev
+  subroutine read_from_checkpoint()
+    if (checkpoint_number > 0) then
+      call chkpnt_init(outdir, n, ch_params, t0, ierr, n_chkpnt=checkpoint_number)
+    else if (restart_time >= 0.0_dp) then
+      call chkpnt_init(outdir, n,  ch_params, t0, ierr, start_before_time=restart_time)
+    endif
+    allocate(c0(n, n))
+    allocate(c1(n, n))
+
+    call read_hdf5_chkpnt(c0, c1, dt, ierr)
+  end subroutine
+
+
   !> @brief Setup grid from given init condition & solve
   subroutine start_from_scratch()
+    if (myrank == 0) then
+      ! initial concentration
+      call setup_grid(c0, grid_res, ch_params, init)
+
+      ! set up HDF5 outputting
+      call output_init(outdir, [2, grid_res], CH_params, ierr)
+    endif
+    
+    call MPI_Barrier(MPI_COMM_WORLD, ierr)
+    call solver_1(Tout, c0, CH_params, selected_solver, ierr)
+  end subroutine
+
+  !> @brief Start with initial conditions set from checkpoint file
+  !! then solve
+  subroutine start_from_checkpoint()
+    if(myrank .ne. 0) then
+      allocate(c1(n, n))
+    end if 
+
+    ! ensure that Tout(1) >= t0
+    do st_tout=1,size(tout)
+      if (tout(st_tout) > t0) then
+        exit
+      endif
+    enddo
+    call MPI_Barrier(MPI_COMM_WORLD, ierr)
+
+    ! Mask time array to only include future times
+    allocate(updated_tout(size(tout)-st_tout))
+    updated_tout = Tout(st_tout:)
+
+    ! Call solver_2 to restart with explicit C and C_prev
+    call solver_2(t0, updated_tout, c0, c1, dt, CH_params, selected_solver, ierr)
   end subroutine
 
 end program main
